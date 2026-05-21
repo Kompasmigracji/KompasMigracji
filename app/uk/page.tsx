ee85e5c73672aa85a7ea4e35a3881b04f24cc90e
AlexKhrysto <iphoenixgsm@gmail.com>
2026-05-21 13:30:05 +0200

    гшг

---
+++ app/uk/page.tsx
@@ -0,0 +1,8 @@
+export default function UkPage() {
+  return (
+    <main style={{ padding: 40, textAlign: 'center' }}>
+      <h1>UK Page</h1>
+      <p>This is a placeholder for the /uk route.</p>
+    </main>
+  );
+}
